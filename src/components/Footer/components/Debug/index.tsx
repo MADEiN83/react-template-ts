acd490d866b28287d2f684748443d44050873c40
Anthony DI STEFANO <ADIS@supralog.local>
2019-09-10 16:56:12 +0200

    Styles and debug component

---
+++ src/components/Footer/components/Debug/index.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+import "./style.scss";
+
+interface IProps {}
+
+const Debug: React.FC<IProps> = (props: IProps) => {
+  const [loadTime, setLoadTime] = useState(0);
+  const [isHidden, setIsHidden] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const loadTime =
+        window.performance.timing.domContentLoadedEventEnd -
+        window.performance.timing.navigationStart;
+      setLoadTime(loadTime);
+    }, 1000);
+  }, []);
+
+  if (isHidden) return <></>;
+
+  return (
+    <div
+      className="debug-container"
+      onClick={() => setIsHidden(true)}
+      title="Click to hide"
+    >
+      <div className="item">DEBUG ONLY</div>
+      <div className="flex">
+        <div className="item">
+          <span className="visible-xs" title="0px to 576px">
+            XS
+          </span>
+          <span className="visible-sm" title="576px to 768px">
+            SM
+          </span>
+          <span className="visible-md" title="768px to 992px">
+            MD
+          </span>
+          <span className="visible-lg" title="992px to 1200px">
+            LG
+          </span>
+          <span className="visible-xl" title="over 1200px">
+            XL
+          </span>
+        </div>
+        <div className="item">{loadTime === 0 ? "..." : `${loadTime}ms`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Debug;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IDefaultProps } from "../../core/interface";
 
 import "./style.scss";
+import Debug from "./components/Debug";
 
 interface IProps extends IDefaultProps {}
 
@@ -9,16 +10,7 @@ const Footer: React.FC<IProps> = (props: IProps) => {
   return (
     <>
       <footer>FOOTER</footer>
-      <span className="visible-xs">XS</span>
-      <span className="visible-sm">SM</span>
-      <span className="visible-md">MD</span>
-      <span className="visible-lg">LG</span>
-      <span className="visible-xl">XL</span>
-      <span className="hidden-xs">hidden XS</span>&nbsp;|&nbsp;
-      <span className="hidden-sm">hidden SM</span>&nbsp;|&nbsp;
-      <span className="hidden-md">hidden MD</span>&nbsp;|&nbsp;
-      <span className="hidden-lg">hidden LG</span>&nbsp;|&nbsp;
-      <span className="hidden-xl">hidden XL</span>
+      <Debug />
     </>
   );
 };
